81ffd97e88be7ec094484b7cf6cce503b0e4b7c7
Trim Bresilla <trim.bresilla@gmail.com>
2022-12-14 23:10:14 +0100

    feat(ALL_GO): Everything seems to work

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <fmt/core.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
-int main(){
-    char x = 'a';
-    fmt::print("{} -> {}\n", x, sizeof(x));
+auto main() -> int {
+    if(glfwInit() == GLFW_FALSE) {
+        fmt::print("Couldn't initialize GLFW");
+        return 1;
+    };
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    auto* window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
+
+    if(window == nullptr){ fmt::print("Coudn't create window"); }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
